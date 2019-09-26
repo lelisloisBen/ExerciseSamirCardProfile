b59d69a9867239586feae852f4a29289acdbb0e8
Samir Benzada <samirbenzada@gmail.com>
2019-09-26 14:56:09 +0000

    project done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-// The two exemple function work...
 function coverChange(val) {
+    let imgSrc = `<img src='https://wallpapercave.com/wp/wp2465958.jpg' />`;
     if (val === "null"|| val === "true") {
-        document.body.querySelector('#coverId').innerHTML = `<img src='https://images.unsplash.com/photo-1511974035430-5de47d3b95da' />`
+        document.body.querySelector('#coverId').innerHTML = imgSrc;
     } else {
         document.body.querySelector('#coverId').innerHTML = '';
     }
@@ -13,7 +13,83 @@ function firstNameChange(val) {
     } else {
         document.body.querySelector('#firstName').innerHTML = val;
     }
-    
+
 }
 
-// write your code below
+function lastNameChange(val) {
+    if (val === '') {
+        document.body.querySelector('#lastName').innerHTML = "Benzada";
+    } else {
+        document.body.querySelector('#lastName').innerHTML = val;
+    }
+}
+
+function socialMediaPositionChange(val) {
+    if (val === '') {
+        document.body.querySelector('#pos').className = "position-right";
+    } else {
+        document.body.querySelector('#pos').className = val;
+    }
+}
+
+function twitterChange(val) {
+    if (val === '') {
+        document.body.querySelector('#twitter').href = "https://twitter.com/";
+    } else {
+        document.body.querySelector('#twitter').href = `https://twitter.com/${val}`;
+    }
+}
+
+function githubChange(val) {
+    if (val === '') {
+        document.body.querySelector('#github').href = "https://github.com/";
+    } else {
+        document.body.querySelector('#github').href = `https://github.com/${val}`;
+    }
+}
+
+function linkedinChange(val) {
+    if (val === '') {
+        document.body.querySelector('#linkedin').href = "https://linkedin.com/";
+    } else {
+        document.body.querySelector('#linkedin').href = `https://linkedin.com/${val}`;
+    }
+}
+
+function instagramChange(val) {
+    if (val === '') {
+        document.body.querySelector('#instagram').href = "https://instagram.com/";
+    } else {
+        document.body.querySelector('#instagram').href = `https://instagram.com/${val}`;
+    }
+}
+
+function roleChange(val) {
+    if (val === '') {
+        document.body.querySelector('#role').innerHTML = "Developer";
+    } else {
+        document.body.querySelector('#role').innerHTML = val;
+    }
+}
+
+function cityChange(val) {
+    if (val === '') {
+        document.body.querySelector('#city').innerHTML = "Miami";
+    } else {
+        document.body.querySelector('#city').innerHTML = val;
+    }
+}
+
+function countryChange(val) {
+    if (val === '') {
+        document.body.querySelector('#country').innerHTML = "USA";
+    } else {
+        document.body.querySelector('#country').innerHTML = val;
+    }
+}
+
+
+
+
+
+
